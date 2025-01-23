74eba588ed1936557b2670d3ea34356dfa9439a1
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-23 20:07:37 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -13,15 +13,15 @@ wget --no-check-certificate -O "${BASE_DR}main_colors.sh" "${BASE_URL}main_color
 download_and_execute() {
     local script_name="$1"
     local url="$2"
-    echo "$(color "blue" "Downloading and executing: ${script_name}")"
+    echo -e "$(color "blue" "Downloading and executing: ${script_name}")"
     
     # Attempt to download the script
     if wget --no-check-certificate -O "${BASE_DR}${script_name}" "${url}"; then
-        echo "$(color "green" "Download successful.")"
+        echo -e "$(color "green" "Download successful.")"
         # Execute the downloaded script
         sh "${BASE_DR}${script_name}"
     else
-        echo "$(color "red" "Download failed.")"
+        echo -e "$(color "red" "Download failed.")"
     fi
 }
 
@@ -30,18 +30,18 @@ menu_option() {
     local description="$1"
     local script_name="$2"
     local url="$3"
-    echo "$(color "white" "${description}")"
+    echo -e "$(color "white" "${description}")"
     read -p "$(color "white" "Do you want to proceed? [y/n]: ")" choice
     case "${choice}" in
         "y") download_and_execute "${script_name}" "${url}" ;;
         "n") return ;;
-        *) echo "$(color "red" "Invalid option. Please try again.")" ;;
+        *) echo -e "$(color "red" "Invalid option. Please try again.")" ;;
     esac
 }
 
 # Function to delete the script and exit
 delete_and_exit() {
-    echo "$(color "red" "Deleting script and exiting.")"
+    echo -e "$(color "red" "Deleting script and exiting.")"
     rm -rf "${BASE_DR}" /usr/bin/confsoft
     exit
 }
@@ -51,24 +51,24 @@ check_openwrt_version() {
     local supported_versions="19 21 22 23 24 SN"
     local release=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
     if echo "${supported_versions}" | grep -q "${release}"; then
-        echo "$(color "white" "OpenWrt version: ${release} - Supported")"
+        echo -e "$(color "white" "OpenWrt version: ${release} - Supported")"
     else
-        echo "$(color "red" "Unsupported OpenWrt version: ${release}")"
-        echo "$(color "white" "Supported versions: ${supported_versions}")"
+        echo -e "$(color "red" "Unsupported OpenWrt version: ${release}")"
+        echo -e "$(color "white" "Supported versions: ${supported_versions}")"
         exit 1
     fi
 }
 
 color_code() {
-for i in `seq 30 38` `seq 40 47` ; do
-    for j in 0 1 2 3 4 5 6 7 ; do
-        printf "\033[${j};${i}m"
-        printf " ${j};${i} "
-        printf "\033[0;39;49m"
-        printf " "
+    for i in `seq 30 38` `seq 40 47` ; do
+        for j in 0 1 2 3 4 5 6 7 ; do
+            printf "\033[${j};${i}m"
+            printf " ${j};${i} "
+            printf "\033[0;39;49m"
+            printf " "
+        done
+        printf "\n"
     done
-    printf "\n"
-done
 }
 
 # Function to display system information
@@ -77,30 +77,30 @@ display_system_info() {
     local available_flash=$(df | awk '/overlayfs:\/overlay/ { print int($4 / 1024) }')
     local usb_devices=$(ls /sys/bus/usb/devices | grep -q usb && echo "Detected" || echo "Not detected")
 
-    echo "$(color "white" "Information")"
-    echo "$(color "white" "Available Memory: ${available_memory} MB")"
-    echo "$(color "white" "Available Flash Storage: ${available_flash} MB")"
-    echo "$(color "white" "USB Devices: ${usb_devices}")"
-    echo "$(color "white" "Directory to install scripts: /etc/config-software/")"
-    echo "$(color "red_white" "Disclaimer: Use this script at your own risk.")"
+    echo -e "$(color "white" "Information")"
+    echo -e "$(color "white" "Available Memory: ${available_memory} MB")"
+    echo -e "$(color "white" "Available Flash Storage: ${available_flash} MB")"
+    echo -e "$(color "white" "USB Devices: ${usb_devices}")"
+    echo -e "$(color "white" "Directory to install scripts: /etc/config-software/")"
+    echo -e "$(color "red_white" "Disclaimer: Use this script at your own risk.")"
 }
 
 # Define Language Selections
-LANGUAGES=("English" "日本語" "中文")
+LANGUAGES=("English" "Japan" "China")
 SELECTED_LANGUAGE="en"  # Default language
 
 # Function to select language
 select_language() {
-    echo "$(color "white" "Select your language:")"
-    echo "$(color "blue" "[e]: English")"
-    echo "$(color "green" "[j]: 日本語")"
-    echo "$(color "red" "[c]: 中文")"
+    echo -e "$(color "white" "Select your language:")"
+    echo -e "$(color "blue" "[e]: English")"
+    echo -e "$(color "green" "[j]: 日本語")"
+    echo -e "$(color "red" "[c]: 中文")"
     read -p "$(color "white" "Choose an option [e/j/c]: ")" lang_choice
     case "${lang_choice}" in
         "e") SELECTED_LANGUAGE="en" ;;
         "j") SELECTED_LANGUAGE="ja" ;;
         "c") SELECTED_LANGUAGE="cn" ;;
-        *) echo "$(color "red" "Invalid choice, defaulting to English.")" ;;
+        *) echo -e "$(color "red" "Invalid choice, defaulting to English.")" ;;
     esac
 }
 
@@ -130,15 +130,15 @@ main_menu() {
         MENU00="スクリプトを終了"
         MENU01="スクリプトを削除して終了"
     elif [ "${SELECTED_LANGUAGE}" = "cn" ]; then
-        MENU0="一体化脚本菜单"
-        MENU1="互联网设置（仅限日本）" 
-        MENU2="初步系统设置"
-        MENU3="安装推荐的软件包"
-        MENU4="安装并配置广告拦截器"
-        MENU5="配置访问点"
-        MENU6="执行其他脚本"
+        MENU0="一体化脚本菜?"
+        MENU1="互?网?置（?限日本）" 
+        MENU2="初?系??置"
+        MENU3="安装推荐的?件包"
+        MENU4="安装并配置广告?截器"
+        MENU5="配置??点"
+        MENU6="?行其他脚本"
         MENU00="退出脚本"
-        MENU01="删除脚本并退出"
+        MENU01="?除脚本并退出"
     fi
 
     TARGET1="internet-config.sh"
@@ -151,17 +151,17 @@ main_menu() {
     TARGET01="delete_and_exit"
     
     while :; do
-        echo "$(color "white" "-------------------------------------------------------")"
-        echo "$(color "white" "${MENU0}")"
-        echo "$(color "blue" "[i]: ${MENU1}")"
-        echo "$(color "yellow" "[s]: ${MENU2}")"
-        echo "$(color "green" "[p]: ${MENU3}")"
-        echo "$(color "magenta" "[b]: ${MENU4}")"
-        echo "$(color "red" "[a]: ${MENU5}")"
-        echo "$(color "cyan" "[o]: ${MENU6}")"
-        echo "$(color "white" "[e]: ${MENU00}")"
-        echo "$(color "white_black" "[d]: ${MENU01}")"
-        echo "$(color "white" "-------------------------------------------------------")"
+        echo -e "$(color "white" "-------------------------------------------------------")"
+        echo -e "$(color "white" "${MENU0}")"
+        echo -e "$(color "blue" "[i]: ${MENU1}")"
+        echo -e "$(color "yellow" "[s]: ${MENU2}")"
+        echo -e "$(color "green" "[p]: ${MENU3}")"
+        echo -e "$(color "magenta" "[b]: ${MENU4}")"
+        echo -e "$(color "red" "[a]: ${MENU5}")"
+        echo -e "$(color "cyan" "[o]: ${MENU6}")"
+        echo -e "$(color "white" "[e]: ${MENU00}")"
+        echo -e "$(color "white_black" "[d]: ${MENU01}")"
+        echo -e "$(color "white" "-------------------------------------------------------")"
         read -p "$(color "white" "Select an option: ")" option
         case "${option}" in
             "i") menu_option "${MENU1}" "${TARGET1}" "${BASE_URL}${TARGET1}?lang=${SELECTED_LANGUAGE}" ;;
@@ -172,7 +172,7 @@ main_menu() {
             "o") menu_option "${MENU6}" "${TARGET6}" "${BASE_URL}${TARGET6}?lang=${SELECTED_LANGUAGE}" ;;
             "e") exit ;;
             "d") delete_and_exit ;;
-            *) echo "$(color "red" "Invalid option. Please try again.")" ;;
+            *) echo -e "$(color "red" "Invalid option. Please try again.")" ;;
         esac
     done
 }

8d207bba123edbbb73cf74ea9e7977017344c4e4
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-24 20:10:35 +0900

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -12,37 +12,13 @@ cat <<"EOF" > /usr/bin/aio
 BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
 BASE_DR="/tmp/config-software2/"
 
-download_script() {
-wget --no-check-certificate -O ${BASE_DR}main-colors.sh ${BASE_URL}main-colors.sh
-wget --no-check-certificate -O ${BASE_DR}openwrt-config.sh ${BASE_URL}openwrt-config.sh
-}
-
-language() {
-LANGUAGE=$1
-echo "選択された言語: $LANGUAGE"
-LANGUAGE=$2
-echo "選択された言語$2: $LANGUAGE"
-
-if [ "$LANGUAGE" = "en" ]; then
-    SELECTED_LANGUAGE="en"
-    export SELECTED_LANGUAGE
-elif [ "$LANGUAGE" = "ja" ]; then
-    SELECTED_LANGUAGE="ja"
-    export SELECTED_LANGUAGE
-else
-    select_language
-    read -p "STOP"
-fi
-}
-
 select_language() {
-. "${BASE_DR}main-colors.sh"
-    echo -e "$(color "white" "-------------------------------------------------------")"
-    echo -e "$(color "white" "Select your language:")"
-    echo -e "$(color "white" "[e]: English")"
-    echo -e "$(color "white" "[j]: 日本語")"
-    echo -e "$(color "white" "-------------------------------------------------------")"
-    read -p "$(color "white" "Choose an option [e/j]: ")" lang_choice
+    echo -e "------------------------------------------------------"
+    echo -e "Select your language:"
+    echo -e "[e]: English"
+    echo -e "[j]: 日本語"
+    echo -e "------------------------------------------------------"
+    read -p "Choose an option [e/j]: " lang_choice
     case "${lang_choice}" in
         "e") SELECTED_LANGUAGE="en" ;;
         "j") SELECTED_LANGUAGE="ja" ;;
@@ -51,12 +27,37 @@ select_language() {
 export SELECTED_LANGUAGE
 }
 
-run_script() {
-sh ${BASE_DR}openwrt-config.sh
-}
+supported_versions="19 21 22 23 24 SN"
+release=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+if echo "${supported_versions}" | grep -q "${release}"; then
+    echo -e "OpenWrt version: ${release} - Supported"
+else
+    echo -e "Unsupported OpenWrt version: ${release}"
+    echo -e "Supported versions: ${supported_versions}"
+    exit 1
+fi
 
-download_script
-language
-run_script
+LANGUAGE=$1
+echo "選択された言語: $LANGUAGE"  # 引数を表示
+
+if [ "$LANGUAGE" = "en" ]; then
+    SELECTED_LANGUAGE="en"
+    export SELECTED_LANGUAGE
+    echo "English selected."
+elif [ "$LANGUAGE" = "ja" ]; then
+    SELECTED_LANGUAGE="ja"
+    export SELECTED_LANGUAGE
+    echo "Japanese selected."
+else
+    select_language
+    echo "Default language selected."
+fi
+
+echo "選択された言語: $SELECTED_LANGUAGE"  # 引数を表示
+read -p "STOP"
+
+wget --no-check-certificate -O ${BASE_DR}main-colors.sh ${BASE_URL}main-colors.sh
+wget --no-check-certificate -O ${BASE_DR}openwrt-config.sh ${BASE_URL}openwrt-config.sh
+sh ${BASE_DR}openwrt-config.sh
 EOF
 chmod +x /usr/bin/aio

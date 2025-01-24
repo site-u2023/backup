307174b5151985a3a1dfcab4557a9b5b2f08b588
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-24 18:03:45 +0900

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -4,8 +4,40 @@ mkdir -p /tmp/config-software2
 
 cat <<"EOF" > /usr/bin/aio
 #!/bin/sh
+# Function to select language
+language() {
+LANGUAGE=$1
+if [ "$LANGUAGE" = "aioen" ]; then
+    SELECTED_LANGUAGE="en"
+    export SELECTED_LANGUAGE
+    download_script
+elif [ "$LANGUAGE" = "aioja" ]; then
+    SELECTED_LANGUAGE="ja"
+    export SELECTED_LANGUAGE
+    download_script
+else
+    select_language
+fi
+}
+select_language() {
+    echo -e "$(color "white" "-------------------------------------------------------")"
+    echo -e "$(color "white" "Select your language:")"
+    echo -e "$(color "white" "[e]: English")"
+    echo -e "$(color "white" "[j]: 日本語")"
+    echo -e "$(color "white" "-------------------------------------------------------")"
+    read -p "$(color "white" "Choose an option [e/j]: ")" lang_choice
+    case "${lang_choice}" in
+        "e") SELECTED_LANGUAGE="en" ;;
+        "j") SELECTED_LANGUAGE="ja" ;;
+        *) echo -e "$(color "red" "Invalid choice, defaulting to English.")" ;;
+    esac
+export SELECTED_LANGUAGE
+}
+download_script() {
 wget --no-check-certificate -O /tmp/config-software2/main-colors.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/main-colors.sh
 wget --no-check-certificate -O /tmp/config-software2/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/openwrt-config.sh
 sh /tmp/config-software2/openwrt-config.sh
+}
+language
 EOF
 chmod +x /usr/bin/aio

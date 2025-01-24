9ed0803eabc258f4df810ce71d8226c4daedbd29
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-24 17:11:12 +0900

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-mkdir -p /etc/config-software2
+mkdir -p /tmp/config-software2
 
 cat <<"EOF" > /usr/bin/aio
 #!/bin/sh
-wget --no-check-certificate -O /etc/config-software2/main-colors.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/main-colors.sh
-wget --no-check-certificate -O /etc/config-software2/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/openwrt-config.sh
-sh /etc/config-software2/openwrt-config.sh
+wget --no-check-certificate -O /tmp/config-software2/main-colors.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/main-colors.sh
+wget --no-check-certificate -O /tmp/config-software2/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software2/main/openwrt-config.sh
+sh /tmp/config-software2/openwrt-config.sh
 EOF
 chmod +x /usr/bin/aio

f73387d41ef0d7057ccd99b95213a5e30f73dfa1
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 23:09:54 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -30,10 +30,10 @@ color() {
 check_version() {
 RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
     if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
-        echo "OpenWrt version: "${RELEASE_VERSION}" - Supported"
+        echo -e "$(color "white" "OpenWrt version: "${RELEASE_VERSION}.00.0" - Supported")"
     else
-        echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
-        echo "Supported versions: ${SUPPORTED_VERSIONS}"
+        echo -e "$(color "red" "Unsupported OpenWrt version: ${RELEASE_VERSION}.00.0")"
+        echo -e "$(color "white" "Supported versions: ${SUPPORTED_VERSIONS}")"
         exit 1
     fi
 }

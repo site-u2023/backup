5a6b73dfc686bbf1f1acea43719c6decdfdb76b3
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 18:17:29 +0900

    Update version.sh

--- version.sh
+++ version.sh
@@ -3,6 +3,7 @@
 # OpenWrt >= 19.07
 
 
+check_version() {
 RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
     if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
         echo "OpenWrt version: ${RELEASE_VERSION}.00.0 - Supported"
@@ -11,3 +12,4 @@ RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | 
         echo "Supported versions: ${SUPPORTED_VERSIONS}"
         exit 1
     fi
+}

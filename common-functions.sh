33ff9cb695d6f51116c318723260d5bc4e5b20b6
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-26 00:32:03 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -39,26 +39,25 @@ RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | 
 }
 
 check_language() {
-if [ "$LANGUAGE" = "en" ]; then
-    SELECTED_LANGUAGE="en"
-elif [ "$LANGUAGE" = "ja" ]; then
-    SELECTED_LANGUAGE="ja"
-else
-    echo -e "$(color "white" "------------------------------------------------------")"
-    echo -e "$(color "white" "Select your language")"
-    echo -e "$(color "blue" "[e]: English")"
-    echo -e "$(color "red" "[j]: 日本語")"
-    echo -e "$(color "white" "------------------------------------------------------")"
-    read -p "Choose an option [e/j]: " lang_choice
-    case "${lang_choice}" in
-        "e") SELECTED_LANGUAGE="en" ;;
-        "j") SELECTED_LANGUAGE="ja" ;;
-        *) 
-            echo "Invalid choice, defaulting to English."
-            SELECTED_LANGUAGE="en"
-            ;;
-    esac
-fi
+    if [ "$LANGUAGE" = "en" ]; then
+        SELECTED_LANGUAGE="en"
+    elif [ "$LANGUAGE" = "ja" ]; then
+        SELECTED_LANGUAGE="ja"
+    else
+        while true; do
+            echo -e "$(color "white" "------------------------------------------------------")"
+            echo -e "$(color "white" "Select your language")"
+            echo -e "$(color "blue" "[en]: English")"
+            echo -e "$(color "red" "[ja]: 日本語")"
+            echo -e "$(color "white" "------------------------------------------------------")"
+            read -p "Choose an option [e/j]: " lang_choice
+            case "${lang_choice}" in
+                "en") SELECTED_LANGUAGE="en"; break ;;
+                "ja") SELECTED_LANGUAGE="ja"; break ;;
+                *) echo "Invalid choice." ;;
+            esac
+        done
+    fi
 }
 
 check_common() {

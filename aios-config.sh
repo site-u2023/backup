b93f31b060ef4fde1ce358f48fa0ac4f981fae6e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 18:34:49 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -8,8 +8,6 @@ BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
 BASE_DIR="/tmp/config-software2/"
 
 mkdir -p "$BASE_DIR"
-wget --no-check-certificate -O "${BASE_DIR}check-version.sh" "${BASE_URL}check-version.sh"
-wget --no-check-certificate -O "${BASE_DIR}check-language.sh" "${BASE_URL}check-language.sh"
 wget --no-check-certificate -O "${BASE_DIR}ttyd.sh" "${BASE_URL}ttyd.sh"
 wget --no-check-certificate -O "/usr/bin/aios" "${BASE_URL}aios"
 chmod +x /usr/bin/aios

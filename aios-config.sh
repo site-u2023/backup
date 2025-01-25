59c753262ede0936bbab61f86de5b4c3aa0b10c9
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 11:22:12 +0900

    Update and rename all-in-one.sh to aios-config.sh

---
+++ aios-config.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# License: CC0
+# OpenWrt >= 19.07
+# This script is specifically designed for the initial setup of an all-in-one script.
+
+
+mkdir -p /tmp/config-software2
+
+BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
+BASE_DIR="/tmp/config-software2/"
+SUPPORTED_VERSIONS="19 21 22 23 24 SN"
+
+    RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+    if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
+        echo "OpenWrt version: ${RELEASE_VERSION}.00.0 - Supported"
+    else
+        echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
+        echo "Supported versions: ${SUPPORTED_VERSIONS}"
+        exit 1
+    fi
+
+export BASE_URL
+export BASE_DIR
+export RELEASE_VERSION
+
+wget --no-check-certificate -O "${BASE_DIR}ttyd.sh" "${BASE_URL}ttyd.sh"
+wget --no-check-certificate -O "/usr/bin/aio/aio" "${BASE_URL}aios"
+chmod +x /usr/bin/aio
+aios

437b0e2a4f901b1eaa0df3e995a4b81f80606e0c
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 20:02:10 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -4,11 +4,22 @@
 # This script is specifically designed for the initial setup of an all-in-one script.
 
 
+SUPPORTED_VERSIONS="19 21 22 23 24 SN"
+RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+    if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
+        echo "OpenWrt version: ${RELEASE_VERSION}.00.0 - Supported"
+    else
+        echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
+        echo "Supported versions: ${SUPPORTED_VERSIONS}"
+        exit 1
+    fi
+
 BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
 BASE_DIR="/tmp/config-software2/"
-
 mkdir -p "$BASE_DIR"
 wget --no-check-certificate -O "${BASE_DIR}ttyd.sh" "${BASE_URL}ttyd.sh"
 wget --no-check-certificate -O "/usr/bin/aios" "${BASE_URL}aios"
 chmod +x /usr/bin/aios
 sh "${BASE_DIR}ttyd.sh"
+
+aios

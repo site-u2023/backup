23c1a2549f4d6a73cc77ee41f42a4edf58b59aeb
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 23:12:54 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -7,7 +7,7 @@
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
 RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
     if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
-        echo "OpenWrt version: ${RELEASE_VERSION}.00.0 - Supported"
+        echo "OpenWrt version: ${RELEASE_VERSION} - Supported"
     else
         echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
         echo "Supported versions: ${SUPPORTED_VERSIONS}"

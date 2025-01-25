5e7d4672f376827dab099b146e2974baf618d00e
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-25 18:07:44 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -7,19 +7,7 @@
 BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
 BASE_DIR="/tmp/config-software2/"
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
-
-    RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
-    if echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
-        echo "OpenWrt version: ${RELEASE_VERSION}.00.0 - Supported"
-    else
-        echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
-        echo "Supported versions: ${SUPPORTED_VERSIONS}"
-        exit 1
-    fi
-
-export BASE_URL
-export BASE_DIR
-export RELEASE_VERSION
+source "${BASE_DR}version.sh"
 
 mkdir -p "$BASE_DIR"
 wget --no-check-certificate -O "${BASE_DIR}ttyd.sh" "${BASE_URL}ttyd.sh"

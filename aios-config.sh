664fb39826d6991612459df20c6cda3237a54400
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-26 11:25:07 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -4,6 +4,9 @@
 # This script is specifically designed for the initial setup of an all-in-one script.
 
 LANGUAGE=$1
+BASE_URL="https://raw.githubusercontent.com/site-u2023/aios/main/"
+BASE_DIR="/tmp/aios/"
+mkdir -p "$BASE_DIR"
 
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
 RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
@@ -13,9 +16,6 @@ RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | 
         exit 1
     fi
     
-BASE_URL="https://raw.githubusercontent.com/site-u2023/config-software2/main/"
-BASE_DIR="/tmp/config-software2/"
-mkdir -p "$BASE_DIR"
 wget --no-check-certificate -O "${BASE_DIR}ttyd.sh" "${BASE_URL}ttyd.sh"
 wget --no-check-certificate -O "/usr/bin/aios" "${BASE_URL}aios"
 chmod +x /usr/bin/aios

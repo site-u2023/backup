715b2cbf504e50a9fbcbdafc6a0c65f112441caf
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-27 00:54:47 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -4,7 +4,7 @@
 # OpenWrt >= 19.07
 # This script is specifically designed for the initial setup of an all-in-one script.
 
-SELECTED_LANGUAGE=$1
+#SELECTED_LANGUAGE=$1
 BASE_URL="https://raw.githubusercontent.com/site-u2023/aios/main/"
 BASE_DIR="/tmp/aios/"
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
@@ -14,7 +14,7 @@ mkdir -p "$BASE_DIR" || {
     exit 1
 }
 
-RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+RELEASE_VERSION=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2) > "${BASE_DIR}check_version"
     if ! echo "${SUPPORTED_VERSIONS}" | grep -q "${RELEASE_VERSION}"; then
         echo "Unsupported OpenWrt version: ${RELEASE_VERSION}"
         echo "Supported versions: ${SUPPORTED_VERSIONS}"
@@ -34,8 +34,8 @@ check_ttyd_installed() {
 wget --no-check-certificate -O "/usr/bin/aios" "${BASE_URL}aios"
 chmod +x /usr/bin/aios
 
-echo "${SELECTED_LANGUAGE}" > "${BASE_DIR}check_language"
-echo "${RELEASE_VERSION}" > "${BASE_DIR}check_version"
+#echo "${SELECTED_LANGUAGE}" > "${BASE_DIR}check_language"
+#echo "${RELEASE_VERSION}" > "${BASE_DIR}check_version"
 
 check_ttyd_installed
-aios
+aios "$1"

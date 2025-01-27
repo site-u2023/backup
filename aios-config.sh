41f76ee9557bf64f13c26c65e3296676c0bdfd59
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-27 21:38:34 +0900

    Update aios-config.sh

--- aios-config.sh
+++ aios-config.sh
@@ -50,10 +50,10 @@ download_and_execute() {
         exit 1
     }
     chmod +x /usr/bin/aios
-    RELEASE_VERSION="${RELEASE_VERSION}" /usr/bin/aios "$SELECTED_LANGUAGE"
     echo -e "\n--- Installation Complete ---"
     echo "The installation of aios has been completed successfully."
     echo "You can now run the 'aios' script from anywhere."
+    RELEASE_VERSION="${RELEASE_VERSION}" /usr/bin/aios "$SELECTED_LANGUAGE"
 }
 
 check_version

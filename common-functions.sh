e61e5a59753de60c5243bdde6ea8e38c63abc5f3
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-29 13:08:34 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -204,7 +204,7 @@ menu_option() {
             if ask_confirmation "download"; then
                 if wget --quiet -O "${BASE_DIR}/${script_name}" "${BASE_URL}/${script_name}"; then
                     show_notification "download_success"
-                    . "${BASE_DIR}$/{script_name}"
+                    . "${BASE_DIR}/${script_name}"
                 else
                     show_notification "download_failure"
                 fi

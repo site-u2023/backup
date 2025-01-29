1de1435c14f85d2ff24826b92db7790b5517921d
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-30 01:02:59 +0900

    Update country_timezone.sh

--- country_timezone.sh
+++ country_timezone.sh
@@ -4,90 +4,93 @@
 
 # タイムゾーンデータ
 country_timezones="
-en US United States UTC-5 UTC-6 UTC-7 UTC-8 UTC-9 UTC-10 UTC-11
-ca CA Canada UTC-3 UTC-4 UTC-5 UTC-6 UTC-7 UTC-8 UTC-9
-ja JP Japan UTC+9
-de DE Germany UTC+1 UTC+2
-nl NL Netherlands UTC+1 UTC+2
-it IT Italy UTC+1 UTC+2
-pt PT Portugal UTC-1 UTC+0 UTC+1
-lu LU Luxembourg UTC+1 UTC+2
-no NO Norway UTC+1 UTC+2
-fi FI Finland UTC+2 UTC+3
-dk DK Denmark UTC+1 UTC+2
-ch CH Switzerland UTC+1 UTC+2
-cz CZ Czech Republic UTC+1 UTC+2
-es ES Spain UTC+1 UTC+2
-gb GB United Kingdom UTC+0 UTC+1
-kr KR Republic of Korea UTC+9
-cn CN China UTC+8
-fr FR France UTC+1 UTC+2
-hk HK Hong Kong UTC+8
-sg SG Singapore UTC+8
-tw TW Taiwan UTC+8
-br BR Brazil UTC-3 UTC-4 UTC-5
-il IL Israel UTC+2 UTC+3
-sa SA Saudi Arabia UTC+3
-lb LB Lebanon UTC+2
-ae AE United Arab Emirates UTC+4
-za ZA South Africa UTC+2
-ar AR Argentina UTC-3
-au AU Australia UTC+8 UTC+9 UTC+10 UTC+11 UTC+12
-at AT Austria UTC+1 UTC+2
-bo BO Bolivia UTC-4
-cl CL Chile UTC-3 UTC-4 UTC-5
-gr GR Greece UTC+2 UTC+3
-is IS Iceland UTC+0
-in IN India UTC+5:30
-ie IE Ireland UTC+0 UTC+1
-kw KW Kuwait UTC+3
-li LI Liechtenstein UTC+1 UTC+2
-lt LT Lithuania UTC+2 UTC+3
-mx MX Mexico UTC-6 UTC-7 UTC-8
-ma MA Morocco UTC+0 UTC+1
-nz NZ New Zealand UTC+12 UTC+13
-pl PL Poland UTC+1 UTC+2
-pr PR Puerto Rico UTC-4
-sk SK Slovak Republic UTC+1 UTC+2
-si SI Slovenia UTC+1 UTC+2
-th TH Thailand UTC+7
-uy UY Uruguay UTC-3
-pa PA Panama UTC-5
-ru RU Russia UTC+3 UTC+4 UTC+5 UTC+6 UTC+7 UTC+8 UTC+9 UTC+10 UTC+11
-eg EG Egypt UTC+2
-tt TT Trinidad and Tobago UTC-4
-tr TR Turkey UTC+3
-cr CR Costa Rica UTC-6
-ec EC Ecuador UTC-5
-hn HN Honduras UTC-6
-ke KE Kenya UTC+3
-ua UA Ukraine UTC+2 UTC+3
-vn VN Vietnam UTC+7
-bg BG Bulgaria UTC+2 UTC+3
-cy CY Cyprus UTC+2 UTC+3
-ee EE Estonia UTC+2 UTC+3
-mu MU Mauritius UTC+4
-ro RO Romania UTC+2 UTC+3
-cs CS Serbia and Montenegro UTC+1 UTC+2
-id ID Indonesia UTC+7 UTC+8 UTC+9
-pe PE Peru UTC-5
-ve VE Venezuela UTC-4
-jm JM Jamaica UTC-5
-bh BH Bahrain UTC+3
-om OM Oman UTC+4
-jo JO Jordan UTC+2
-bm BM Bermuda UTC-4
-co CO Colombia UTC-5
-do DO Dominican Republic UTC-4
-gt GT Guatemala UTC-6
-ph PH Philippines UTC+8
-lk LK Sri Lanka UTC+5:30
-sv SV El Salvador UTC-6
-tn TN Tunisia UTC+1
-pk PK Pakistan UTC+5
-qa QA Qatar UTC+3
-dz DZ Algeria UTC+1
-
+Saudi_Arabia ar SA UTC+3
+Bulgaria bg BG UTC+2 UTC+3
+Bangladesh bn BD UTC+6
+Canada ca CA UTC-3 UTC-4 UTC-5 UTC-6 UTC-7 UTC-8 UTC-9
+Czech_Republic cs CZ UTC+1 UTC+2
+Denmark da DK UTC+1 UTC+2
+Germany de DE UTC+1 UTC+2
+Greece el GR UTC+2 UTC+3
+United_States en US UTC-5 UTC-6 UTC-7 UTC-8 UTC-9 UTC-10 UTC-11
+Spain es ES UTC+1 UTC+2
+Finland fi FI UTC+2 UTC+3
+France fr FR UTC+1 UTC+2
+Israel he IL UTC+2 UTC+3
+India hi IN UTC+5:30
+Hungary hu HU UTC+1 UTC+2
+Italy it IT UTC+1 UTC+2
+Japan ja JP UTC+9
+Republic_of_Korea ko KR UTC+9
+India mr IN UTC+5:30
+Malaysia ms MY UTC+8
+Netherlands nl NL UTC+1 UTC+2
+Norway no NO UTC+1 UTC+2
+Poland pl PL UTC+1 UTC+2
+Portugal pt PT UTC-1 UTC+0 UTC+1
+Brazil pt-br BR UTC-3 UTC-4 UTC-5
+Romania ro RO UTC+2 UTC+3
+Russia ru RU UTC+3 UTC+4 UTC+5 UTC+6 UTC+7 UTC+8 UTC+9 UTC+10 UTC+11
+Slovak_Republic sk SK UTC+1 UTC+2
+Sweden sv SE UTC+1 UTC+2
+Turkey tr TR UTC+3
+Ukraine uk UA UTC+2 UTC+3
+Vietnam vi VN UTC+7
+China zh-cn CN UTC+8
+Taiwan zh-tw TW UTC+8
+Luxembourg xx LU UTC+1 UTC+2
+Switzerland xx CH UTC+1 UTC+2
+United_Kingdom xx GB UTC+0 UTC+1
+Hong_Kong xx HK UTC+8
+Singapore xx SG UTC+8
+United_Arab_Emirates xx AE UTC+4
+South_Africa xx ZA UTC+2
+Argentina xx AR UTC-3
+Australia xx AU UTC+8 UTC+9 UTC+10 UTC+11 UTC+12
+Austria xx AT UTC+1 UTC+2
+Bolivia xx BO UTC-4
+Chile xx CL UTC-3 UTC-4 UTC-5
+Iceland xx IS UTC+0
+Ireland xx IE UTC+0 UTC+1
+Kuwait xx KW UTC+3
+Liechtenstein xx LI UTC+1 UTC+2
+Lithuania xx LT UTC+2 UTC+3
+Mexico xx MX UTC-6 UTC-7 UTC-8
+Morocco xx MA UTC+0 UTC+1
+New_Zealand xx NZ UTC+12 UTC+13
+Puerto_Rico xx PR UTC-4
+Slovenia xx SI UTC+1 UTC+2
+Thailand xx TH UTC+7
+Uruguay xx UY UTC-3
+Panama xx PA UTC-5
+Egypt xx EG UTC+2
+Trinidad_and_Tobago xx TT UTC-4
+Costa_Rica xx CR UTC-6
+Ecuador xx EC UTC-5
+Honduras xx HN UTC-6
+Kenya xx KE UTC+3
+Cyprus xx CY UTC+2 UTC+3
+Estonia xx EE UTC+2 UTC+3
+Mauritius xx MU UTC+4
+Serbia_and_Montenegro xx CS UTC+1 UTC+2
+Indonesia xx ID UTC+7 UTC+8 UTC+9
+Peru xx PE UTC-5
+Venezuela xx VE UTC-4
+Jamaica xx JM UTC-5
+Bahrain xx BH UTC+3
+Oman xx OM UTC+4
+Jordan xx JO UTC+2
+Bermuda xx BM UTC-4
+Colombia xx CO UTC-5
+Dominican_Republic xx DO UTC-4
+Guatemala xx GT UTC-6
+Philippines xx PH UTC+8
+Sri_Lanka xx LK UTC+5:30
+El_Salvador xx SV UTC-6
+Tunisia xx TN UTC+1
+Pakistan xx PK UTC+5
+Qatar xx QA UTC+3
+Algeria xx DZ UTC+1
 "
 
 # 引数チェック

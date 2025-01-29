5b17ae1d039ab67cbf7011eed48bec370efa1103
site_u <140032047+site-u2023@users.noreply.github.com>
2025-01-30 02:39:59 +0900

    Update country_timezone.sh

--- country_timezone.sh
+++ country_timezone.sh
@@ -137,11 +137,12 @@ else
 fi
 }
 
-check_country_code "$1"
-country_timezones_data
-check_country_code_data "$1"
 BASE_DIR="${BASE_DIR:-/tmp/aios}"
 ZONEDATA=$(sh ${BASE_DIR}/country_timezone.sh ja)
 read -p $ZONEDATA
+check_common "$1"
+check_country_code "$1"
+country_timezones_data
+check_country_code_data "$1"
 # check_country_code2 "$1"  "$2"
 # check_country_code_data2 "$1" "$2"

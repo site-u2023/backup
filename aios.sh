baa4e3c3cce1bfa3bb34ba46f1335bf05d1ee792
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-01 00:35:43 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -6,7 +6,6 @@
 BASE_URL="https://raw.githubusercontent.com/site-u2023/aios/main"
 BASE_DIR="/tmp/aios"
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
-#SELECTED_LANGUAGE=$1
 
 check_version() {
     RELEASE_VERSION=$(awk -F"'" '/DISTRIB_RELEASE/ {print $2}' /etc/openwrt_release | cut -c 1-2)

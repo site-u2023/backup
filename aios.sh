25018d715dbee955d52c766d593d321d30b43506
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-01 14:33:37 +0900

    Update aios.sh

--- aios.sh
+++ aios.sh
@@ -6,12 +6,12 @@
 BASE_URL="https://raw.githubusercontent.com/site-u2023/aios/main"
 BASE_DIR="/tmp/aios"
 SUPPORTED_VERSIONS="19 21 22 23 24 SN"
+INPUT_LANG=$1
 
 check_version() {
     RELEASE_VERSION=$(awk -F"'" '/DISTRIB_RELEASE/ {print $2}' /etc/openwrt_release | cut -c 1-2)
     if echo "${SUPPORTED_VERSIONS}" | grep -qw "${RELEASE_VERSION}"; then
         echo "${RELEASE_VERSION}" > ${BASE_DIR}/check_version
-        INPUT_LANG=$1
         INPUT_LANG=$(echo "$INPUT_LANG" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tr -d '\n')
         echo "${INPUT_LANG}" > ${BASE_DIR}/check_language
     else

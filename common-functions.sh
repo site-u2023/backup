e8361f1a4c87b13d0bd97c2f3848a2b0ca85ac0c
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-02 00:30:52 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -68,25 +68,31 @@ check_package_manager() {
 }
 
 check_common() {
+
     # バージョン情報の取得
-    [ -f "${BASE_DIR}/check_version" ] && read -r RELEASE_VERSION < "${BASE_DIR}/check_version"
+    if [ -f "${BASE_DIR}/check_version" ]; then
+        RELEASE_VERSION=$(cat "${BASE_DIR}/check_version")
+    fi
     [ -z "$RELEASE_VERSION" ] && check_version
 
     # パッケージ情報の取得
-    [ -f "${BASE_DIR}/check_package_manager" ] && read -r PACKAGE_MANAGER < "${BASE_DIR}/check_package_manager"
+    if [ -f "${BASE_DIR}/check_package_manager" ]; then
+        PACKAGE_MANAGER=$(cat "${BASE_DIR}/check_package_manager")
+    fi
     [ -z "$PACKAGE_MANAGER" ] && check_package_manager  
 
-    # 言語と国の情報が既に存在する場合は再取得しない
+    # カントリー選択の判定 
     if [ -f "${BASE_DIR}/check_language" ] && [ -f "${BASE_DIR}/check_country" ]; then
-        read -r SELECTED_LANGUAGE < "${BASE_DIR}/check_language"
-        read -r SELECTED_COUNTRY < "${BASE_DIR}/check_country"
+        SELECTED_LANGUAGE=$(cat "${BASE_DIR}/check_language")
+        SELECTED_COUNTRY=$(cat "${BASE_DIR}/check_country")
         echo "Using previously selected language and country."
         normalize_language
         return
     fi
 
-    INPUT_LANG="$1"
-    INPUT_LANG=$(echo "$INPUT_LANG" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+    INPUT_LANG=$1
+    INPUT_LANG=$(echo "$INPUT_LANG" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tr -d '\n')
+    echo "Input Language: $INPUT_LANG"
 
     if [ -n "$INPUT_LANG" ]; then
         found_entries=$(sh /tmp/aios/country-timezone.sh "$INPUT_LANG")
@@ -95,7 +101,9 @@ check_common() {
         if [ "$num_matches" -gt 1 ]; then
             echo "Multiple matches found. Please select:"
             select found_entry in $found_entries; do
-                [ -n "$found_entry" ] && break
+                if [ -n "$found_entry" ]; then
+                    break
+                fi
                 echo "Invalid selection. Please try again."
             done
         else
@@ -112,7 +120,6 @@ check_common() {
     else
         check_language    
     fi
-
     normalize_language
 }
 

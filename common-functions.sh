dd339468fbecc675d6451a207419df893db2f659
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-02 13:40:45 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -5,7 +5,7 @@
 BASE_URL="${BASE_URL:-https://raw.githubusercontent.com/site-u2023/aios/main}"
 BASE_DIR="${BASE_DIR:-/tmp/aios}"
 SUPPORTED_VERSIONS="${SUPPORTED_VERSIONS:-19 21 22 23 24 SN}"
-SUPPORTED_LANGUAGES="${SUPPORTED_LANGUAGES:-en ja}"
+SUPPORTED_LANGUAGES="${SUPPORTED_LANGUAGES:-en}"
 
 color_code_map() {
   local color=$1

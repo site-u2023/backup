cb1c1e1d7d95f10367f02d80d6afc524ed452a36
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-02 18:36:26 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -131,6 +131,106 @@ display_info() {
 }
 
 #########################################################################
+# main_menu
+#  メインメニューを表示し、ユーザーの選択を受け付ける。
+#  各メニュー項目の選択に応じて、対応する処理 (menu_option) を呼び出す。
+#########################################################################
+main_menu() {
+    local lang="$SELECTED_LANGUAGE"
+    local MENU1 MENU2 MENU3 MENU4 MENU5 MENU6 MENU00 MENU01 MENU02 SELECT1
+    local ACTION1 ACTION2 ACTION3 ACTION4 ACTION5 ACTION6 ACTION00 ACTION01 ACTION02
+    local TARGET1 TARGET2 TARGET3 TARGET4 TARGET5 TARGET6 TARGET00 TARGET01 TARGET02
+    local option
+
+    case "$lang" in
+        en)
+            MENU1="Internet settings (Japan Only)"
+            MENU2="Initial System Settings"
+            MENU3="Recommended Package Installation"
+            MENU4="Ad blocker installation settings"
+            MENU5="Access Point Settings"
+            MENU6="Other Script Settings"
+            MENU00="Exit Script"
+            MENU01="Remove script and exit"
+            MENU02="country code"
+            SELECT1="Select an option: "
+            ;;
+        ja)
+            MENU1="インターネット設定"
+            MENU2="システム初期設定"
+            MENU3="推奨パッケージインストール"
+            MENU4="広告ブロッカーインストール設定"
+            MENU5="アクセスポイント設定"
+            MENU6="その他のスクリプト設定"
+            MENU00="スクリプト終了"
+            MENU01="スクリプト削除終了"
+            MENU02="カントリーコード"
+            SELECT1="選択してください: "
+            ;;
+        zh-cn)
+            MENU1="互联网设置 (陕西一地区)"
+            MENU2="系统初始设置"
+            MENU3="推荐安装包"
+            MENU4="广告拦截器设置"
+            MENU5="访问点设置"
+            MENU6="其他脚本设置"
+            MENU00="退出脚本"
+            MENU01="删除脚本并退出"
+            MENU02="国码"
+            SELECT1="选择一个选项: "
+            ;;
+        zh-tw)
+            MENU1="網路設定 (日本限定)"
+            MENU2="系統初始設定"
+            MENU3="推薦包對應"
+            MENU4="廣告防錯設定"
+            MENU5="連線點設定"
+            MENU6="其他脚本設定"
+            MENU00="退出脚本"
+            MENU01="移除脚本並退出"
+            MENU02="國碼"
+            SELECT1="選擇一個選項: "
+            ;;
+    esac
+
+    ACTION1="download" ; TARGET1="internet-config.sh"
+    ACTION2="download" ; TARGET2="system-config.sh"
+    ACTION3="download" ; TARGET3="package-config.sh"
+    ACTION4="download" ; TARGET4="ad-dns-blocking-config.sh"
+    ACTION5="download" ; TARGET5="accesspoint-config.sh"
+    ACTION6="download" ; TARGET6="etc-config.sh"
+    ACTION00="exit"
+    ACTION01="delete"
+    ACTION02="download" ; TARGET02="country_timezone.sh"
+
+    while :; do
+        echo -e "$(color "white" "------------------------------------------------------")"
+        echo -e "$(color "blue" "[i]: ${MENU1}")"
+        echo -e "$(color "yellow" "[s]: ${MENU2}")"
+        echo -e "$(color "green" "[p]: ${MENU3}")"
+        echo -e "$(color "magenta" "[b]: ${MENU4}")"
+        echo -e "$(color "red" "[a]: ${MENU5}")"
+        echo -e "$(color "cyan" "[o]: ${MENU6}")"
+        echo -e "$(color "white" "[e]: ${MENU00}")"
+        echo -e "$(color "white_black" "[d]: ${MENU01}")"
+        echo -e "$(color "white" "------------------------------------------------------")"
+        read -p "$(color "white" "${SELECT1}")" option
+        case "${option}" in
+            "i") menu_option "${ACTION1}" "${MENU1}" "${TARGET1}" ;;
+            "s") menu_option "${ACTION2}" "${MENU2}" "${TARGET2}" ;;
+            "p") menu_option "${ACTION3}" "${MENU3}" "${TARGET3}" ;;
+            "b") menu_option "${ACTION4}" "${MENU4}" "${TARGET4}" ;;
+            "a") menu_option "${ACTION5}" "${MENU5}" "${TARGET5}" ;;
+            "o") menu_option "${ACTION6}" "${MENU6}" "${TARGET6}" ;;
+            "e") menu_option "${ACTION00}" "${MENU00}" ;;
+            "d") menu_option "${ACTION01}" "${MENU01}" ;;
+            "cc") menu_option "${ACTION02}" "${MENU02}" "${TARGET02}" ;;
+            *) echo -e "$(color "red" "Invalid option. Please try again.")" ;;
+        esac
+    done
+}
+
+#########################################################################
 # メイン処理の開始
 #  以下の処理を順次実行して、システム情報表示およびメインメニューを起動する。
 #########################################################################

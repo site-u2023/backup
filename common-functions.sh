ee89fb02f81560fd1e5935882b3bbe704f75e581
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-04 02:32:34 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -6,7 +6,7 @@
 #
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 #
-echo common-functions.sh Last update 202502031310-14
+echo common-functions.sh Last update 202502031310-15
 
 # 基本定数の設定
 BASE_URL="${BASE_URL:-https://raw.githubusercontent.com/site-u2023/aios/main}"
@@ -313,7 +313,7 @@ menu_option() {
         "return")
             if ask_confirmation "return"; then
                 show_notification "return"
-                unset -f get_message_Internet_config
+                #unset -f get_message_Internet_config
                 return 0 2>/dev/null || exit 0
             else
                 show_notification "return_cancelled"

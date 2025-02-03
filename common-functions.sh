af3fed0b60b63404b08b436007a8d9c64b0c6912
site_u <140032047+site-u2023@users.noreply.github.com>
2025-02-04 03:00:52 +0900

    Update common-functions.sh

--- common-functions.sh
+++ common-functions.sh
@@ -6,7 +6,7 @@
 #
 # 各種共通処理（ヘルプ表示、カラー出力、システム情報確認、言語選択、確認・通知メッセージの多言語対応など）を提供する。
 #
-echo common-functions.sh Last update 202502031310-18
+echo common-functions.sh Last update 202502031310-19
 
 # 基本定数の設定
 BASE_URL="${BASE_URL:-https://raw.githubusercontent.com/site-u2023/aios/main}"
